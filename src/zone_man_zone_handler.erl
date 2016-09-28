cf4326a4ac376b7be86b4f4505f64caca60976e1
Philip Cristiano <philipcristiano@gmail.com>
2016-09-28 16:54:56 -0400

    Fix zoneadm arg

--- src/zone_man_zone_handler.erl
+++ src/zone_man_zone_handler.erl
@@ -15,7 +15,7 @@ content_types_provided(Req, State) ->
 	], Req, State}.
 
 zones(Req, State) ->
-    Zones = zone_man_cmd:run("/usr/sbin/zoneadm", ["lists", "-v"]),
+    Zones = zone_man_cmd:run("/usr/sbin/zoneadm", ["list", "-v"]),
     Data = [{<<"zones">>, Zones}],
     Body = jsx:encode(Data),
     {Body, Req, State}.
