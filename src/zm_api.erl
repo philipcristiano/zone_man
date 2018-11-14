2d0534ad053545e855a2a81eab3f52156ca233bb
Philip Cristiano <philipcristiano@philipcristiano.com>
2018-11-14 06:08:06 -0500

    Handler: Add post with validation that key exists

--- src/zm_api.erl
+++ src/zm_api.erl
@@ -1,7 +1,7 @@
 -module(zm_api).
 -compile({parse_transform, lager_transform}).
 
--export([upgrade/4]).
+-export([upgrade/4, validate_spec_to_data/2]).
 
 
 -callback get(any(), any()) -> {any(), any(), any()}.
@@ -22,4 +22,48 @@ call(Req, Env, Handler, HandlerState) ->
   {ok, Req3, Result}.
 
 call(<<"GET">>, Req, _Env, Handler, HandlerState) ->
-  Handler:get(Req, HandlerState).
+  Handler:get(Req, HandlerState);
+call(<<"POST">>, Req, _Env, Handler, HandlerState) ->
+  Spec = Handler:post_spec(),
+  {Data, _Errors} = spec_to_data(Spec, Req),
+  Handler:post(Req, HandlerState, Data).
+
+spec_to_data(Spec, Req) ->
+  Bin = read_body(Req),
+  Data = jsx:decode(Bin, [return_maps]),
+  validate_spec_to_data(Spec, Data).
+
+validate_spec_to_data(Spec, Data) ->
+  validate_spec_to_data(Spec, Data, []).
+
+validate_spec_to_data(Spec, Data, _Errors) when is_map(Spec) ->
+  SpecL = maps:to_list(Spec),
+  validate_spec_to_data(SpecL, Data, #{});
+validate_spec_to_data(Spec=[], _Data, _Errors) when is_list(Spec) ->
+  {#{}, []};
+validate_spec_to_data(Spec=[{H, SingleSpec}| T], Data, Errors) when is_list(Spec) ->
+  lager:info("~p ~p", [H, SingleSpec]),
+  {OurData, OurErrors} = case maps:is_key(H, Data) of
+    true -> Val = maps:get(H, Data),
+            {#{H => Val}, []};
+    false ->
+            {#{}, [{field_missing, H}]}
+  end,
+
+  {NextData, NextErrors} = validate_spec_to_data(T, Data, Errors),
+  ReturnData = maps:merge(OurData, NextData),
+  ReturnErrors = OurErrors ++ NextErrors,
+  {ReturnData, ReturnErrors}.
+
+
+read_body(Req) ->
+  read_body(more, Req).
+
+read_body(ok, _Req) ->
+  <<>>;
+read_body(more, Req) ->
+  {Status, OurBin, Req1} = cowboy_req:read_body(Req),
+  lager:info(OurBin),
+  MoreBin = read_body(Status, Req1),
+  <<OurBin/binary, MoreBin/binary>>.
+
